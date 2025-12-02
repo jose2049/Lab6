065428b2e47a543bab308f12340fb437d00ba1b0
Jose Alvarez <g00463862@atu.ie>
2025-12-02 23:25:52 +0000

    code finished

--- src/main/java/atu/ie/BankAccount.java
+++ src/main/java/atu/ie/BankAccount.java
@@ -9,10 +9,10 @@ public class BankAccount {
     private String name;
     private double balance;
 
-    //Constructor for bank account. It initializes the methods from the class BankAccount with a default values
+    // Constructor for BankAccount. Initializes the object's fields with the provided values.
     public  BankAccount(String accNo, String name, double balance) {
 
-        //If balance is = 0 throw an error message
+        // Balance must be a positive amount; throw an exception if <= 0
         if( balance <= 0 ){
             throw new IllegalArgumentException("Balance must be greater than 0");
         }
@@ -40,6 +40,7 @@ public class BankAccount {
         return balance;
     }
 
+
     //addDeposilt adds amount to balance and returns the updated balance
     public double addDeposit(double amount){
 
@@ -50,12 +51,15 @@ public class BankAccount {
         return balance;
     }
 
-    //Withdraw first checks if requested amount exceeds the current balance. If so, throws and exception
+    //This function checks whether the amount selected by the user less than 0
+    //Withdraw checks if requested amount exceeds the current balance. If so, throws and exception
     //Then subtracts the amount and returns the new balance
     public double withdraw(double amount){
-
+        if (amount < 0){
+            throw new IllegalArgumentException("Amount must be greater than 0");
+        }
         if( amount > balance ){
-            throw new IllegalArgumentException("You can't withdraw more than balance, please try again ");
+            throw new IllegalArgumentException("You can't withdraw more than balance, please try again");
         }
         balance = balance - amount;
         return balance;
